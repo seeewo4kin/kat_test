d673d20d9b7e79a02b4b225b6dd39e0e53b7e344
seeewo4kin <sewo4kin321@gmail.com>
2023-10-04 19:01:36 +0300

    need to fix buhbs

---
+++ src/Rome.java
@@ -0,0 +1,64 @@
+public class Rome {
+
+    public static boolean isRome(String[] numbers) throws Exception {
+        boolean firstRoman = false;
+        boolean secondRoman = false;
+        String[] romanNumbers = new String[]{"0", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+        int num1;
+        int num2;
+
+        for (int nLength = 0; nLength < numbers.length; nLength++) {
+            for (int rnLength = 0; rnLength < romanNumbers.length; rnLength++) {
+                if (numbers[nLength].contains(romanNumbers[rnLength]) && nLength == 0) {
+                    firstRoman = true;
+                }
+                if (numbers[nLength].contains(romanNumbers[rnLength]) && nLength == 1) {
+                    secondRoman = true;
+
+                }
+            }
+            if (firstRoman && secondRoman) {
+                return true;
+            } else if (firstRoman && !secondRoman) throw new Exception("Оба числа должны быть одного формата");
+            else if (!firstRoman && secondRoman) throw new Exception("Оба числа должны быть одного формата");
+        }
+        return false;
+    }
+
+    public static int firstToArabic(String[] numbers) {
+        String[] romanNumbers = new String[]{"0", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+        for (int rnLength = 0; rnLength < romanNumbers.length; rnLength++) {
+            if (numbers[0].contains(romanNumbers[rnLength])) {
+                return rnLength;
+
+            }
+        }
+        return 0;
+    }
+    public static int secondToArabic(String[] numbers){
+        String[] romanNumbers = new String[]{"0", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+        for (int rnLength = 0; rnLength < romanNumbers.length; rnLength++) {
+            if (numbers[1].contains(romanNumbers[rnLength])) {
+                return rnLength;
+            }
+        }
+        return 0;
+    }
+    public static String ArabicToRome(int result){
+        String answer = null;
+        String[] romanNumbers = new String[]{"0", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X",
+                "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
+                "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
+                "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX",
+                "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XXX",
+                "XXXXI", "XXXXII", "XXXXIII", "XXXXIV", "XXXXV", "XXXXVI", "XXXXVII", "XXXXVIII", "XXXXIX", "XL",
+                "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L",
+                "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX",
+                "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX",
+                "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX",
+                "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC",
+                "XC", "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"};
+        answer = romanNumbers[result];
+        return answer;
+    }
+}
---
+++ src/kata_Test.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+import java.lang.Exception;
+import java.lang.String;
+
+class kata_Test {
+
+    public static String calc() throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите выражение.");
+        String input = scanner.nextLine();
+        int num1 = 0;
+        int num2 = 0;
+        String answer = null;
+        int result = 0;
+
+
+        String[] numbers = input.split("[+\\-*/]");
+        if (numbers.length != 2) throw new Exception("Должно быть два числа.");
+        boolean isRoman = Rome.isRome(numbers);
+        String oper = kata_Test.findoperation(input);
+        if (isRoman) {
+            num1 = Rome.firstToArabic(numbers);
+            num2 = Rome.secondToArabic(numbers);
+            if(num1 > 10 || num2 > 10) throw new Exception("Числа не должны быть больше 10.");
+            if (oper.equals("+")){
+                result = num1 + num2;
+            }
+            else if (oper.equals("-")){
+                if (num1 > num2) throw new Exception("Ответ не может быть меньше 0");
+                result = num1 - num2;
+            }
+            else if (oper.equals("*")){
+                result = num1 * num2;
+            }
+            else if (oper.equals("/")){
+                result = num1 / num2;
+            }
+            return Rome.ArabicToRome(result);
+        }
+        else {
+            num1 = Integer.parseInt(numbers[0]);
+            num2 = Integer.parseInt(numbers[1]);
+            if (oper.equals("+")){
+                result = num1 + num2;
+            }
+            else if (oper.equals("-")){
+                result = num1 - num2;
+            }
+            else if (oper.equals("*")){
+                result = num1 * num2;
+            }
+            else if (oper.equals("/")){
+                result = num1 / num2;
+            }
+            return "" + result;
+        }
+    }
+
+    private static String findoperation(String input) {
+        if (input.contains("+")) {
+            return "+";
+        } else if (input.contains("-")) {
+            return "-";
+        } else if (input.contains("*")) {
+            return "*";
+        } else if (input.contains("/")) {
+            return "/";
+        }
+        return "";
+    }
+}
